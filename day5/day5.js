c032b9cc45c975d55d6f3e883a52e33f91627be2
Andrea Soloko <asoloko@reigningchamps.com>
2020-12-22 09:48:08 -0600

    Day 5 part 1 and 2 complete

--- day5/day5.js
+++ day5/day5.js
@@ -28,11 +28,34 @@ const tickets = () => {
   })
 }
 
-const boarding = () => {
+const boarding1 = () => {
   const seatCodes = tickets().map(ticket => {
     return ticket.code;
-  })
-  return Math.max(...seatCodes)
+  });
+  return Math.max(...seatCodes);
 }
-// 1015 too high
-console.log(boarding())
+
+const boarding2 = () => {
+  let seatCodes = tickets().map(ticket => {
+    return ticket.code;
+  });
+  seatCodes = seatCodes.filter(code => {
+    if (
+      (seatCodes.includes(code + 1) && !seatCodes.includes(code - 1)) ||
+      (!seatCodes.includes(code + 1) && seatCodes.includes(code - 1))
+    ){
+      return 1;
+    } else {
+      return 0;
+    }
+  }).sort();
+  for (let i =0; i < seatCodes.length - 1; i++){
+    if (seatCodes[i + 1] - seatCodes[i] === 2){
+      return seatCodes[i] + 1;
+    }
+  }
+  return -1;
+}
+
+console.log('Part 1: ', boarding1())
+console.log('Part2: ', boarding2())
